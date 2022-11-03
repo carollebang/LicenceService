8dc6cc73b7148ce6f7fc0764511a7879b527453e
carol <tlebang021@student.wethinkcode.co.za>
2022-11-03 20:46:01 +0200

    "working on Tables"

---
+++ src/test/java/Tuwani/LicenceService/LicenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Tuwani.LicenceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
